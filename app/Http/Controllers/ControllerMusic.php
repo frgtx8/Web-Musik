5fcc2cacd53281349592f7a26907ad82993a01c0
frgtx8 <rahmanfadil274@gmail.com>
2024-11-18 10:55:19 +0700

    Update ControllerMusic.php

--- app/Http/Controllers/ControllerMusic.php
+++ app/Http/Controllers/ControllerMusic.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Music;
 use App\Models\Penyanyi;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ControllerMusic extends Controller
@@ -13,5 +14,23 @@ class ControllerMusic extends Controller
         $penyanyi = Penyanyi::with('relasi')->get();
         return view('home', compact('penyanyi'));
     }
-    //
+    public function login()
+    {
+        return view('login');
+    }
+    public function pencarian()
+    {
+        return view('pencarian');
+    }
+    public function register()
+    {
+        return view('register');
+    }
+    public function storeRegister(Request $request)
+    {
+        $data = $request->validate(['name' => 'string', 'email' => 'email', 'password' => 'min:2']);
+        User::create($data);
+        return redirect('login');
+    }
 }
+
