2090aea3d431315599d307d0e76f23b6c442a0c3
frgtx8 <rahmanfadil274@gmail.com>
2024-11-18 10:58:28 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [ControllerMusic::class, 'index']);
+Route::get('/login', [ControllerMusic::class, 'login']);
+Route::get('/pencarian', [ControllerMusic::class, 'pencarian']);
+Route::get('/register', [ControllerMusic::class, 'register']);
+Route::post('/register', [ControllerMusic::class, 'storeRegister']);
